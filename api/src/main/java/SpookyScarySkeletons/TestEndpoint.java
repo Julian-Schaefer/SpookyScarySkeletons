6200674acf4a77d83e154406e6d363623aca04c0
Julian Schäfer <julian.david.schaefer@gmail.com>
2019-11-27 14:45:19 +0100

    Added API Component

---
+++ api/src/main/java/SpookyScarySkeletons/TestEndpoint.java
@@ -0,0 +1,21 @@
+package SpookyScarySkeletons;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+// URL zum Testen: http://localhost:8080/api/helloworld
+
+@Path("/helloworld")
+@Consumes({ "application/json" })
+@Produces({ "application/json" })
+public class TestEndpoint {
+
+    @GET
+    public Response getHelloWorld() {
+        return Response.ok().entity("Hello World!").build();
+    }
+
+}
