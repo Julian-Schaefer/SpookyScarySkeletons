9f32f81a2b3ba1b8f2aa5fe7dcc891b7ebecd573
Julian Schäfer <julian.david.schaefer@gmail.com>
2019-12-06 15:30:09 +0100

    DB Connection

---
+++ persistenzlogik/src/main/java/SpookyScarySkeletons/persistenzlogik/Account.java
@@ -0,0 +1,31 @@
+package SpookyScarySkeletons.persistenzlogik;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String username;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
