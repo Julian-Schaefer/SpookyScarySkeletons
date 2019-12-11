477818d46d0e4a21c31bb4508c227e181d45e17c
Julian Schäfer <julian.david.schaefer@gmail.com>
2019-12-11 11:08:17 +0100

    Websocket

---
+++ api/src/main/java/SpookyScarySkeletons/api/WebSocketEndpoint.java
@@ -0,0 +1,34 @@
+package SpookyScarySkeletons.api;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+// URL zum Testen: ws://localhost:8080/api/actions
+
+@ServerEndpoint("/websocket")
+public class WebSocketEndpoint {
+
+    @OnOpen
+    public void open(Session session) {
+        System.out.println("Open");
+    }
+
+    @OnClose
+    public void close(Session session) {
+        System.out.println("close");
+    }
+
+    @OnError
+    public void onError(Throwable error) {
+        System.out.println("error");
+    }
+
+    @OnMessage
+    public void handleMessage(String message, Session session) {
+        System.out.println("handle");
+    }
+}
